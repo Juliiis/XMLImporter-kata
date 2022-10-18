a57c7aadb8ef81dc8aeb1f233dcd4e135bd4e190
julieta <julieta.martin@clarity.ai>
2022-10-18 18:12:33 +0200

    refactor: extract insert methods and remove comments

--- XMLImporter/src/main/java/BatchXmlImporterDataBase.java
+++ XMLImporter/src/main/java/BatchXmlImporterDataBase.java
@@ -3,7 +3,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import xmlmodels.Company;
 import xmlmodels.Staff;
 
@@ -14,30 +13,16 @@ public class BatchXmlImporterDataBase {
 
       final int companyId;
 
-      companyId = insertCompanyValues(company, connection);
+      companyId = BatchXmlImporterInserts.insertCompanyValues(company, connection);
 
-      // recorre un staff de company.staff y setea los valores de staff y de salary
       for (Staff staff : company.staff) {
-        insertStaffValues(connection, companyId, staff);
-        insertSalaryValues(connection, staff);
+        BatchXmlImporterInserts.insertStaffValues(connection, companyId, staff);
+        BatchXmlImporterInserts.insertSalaryValues(connection, staff);
       }
     }
   }
 
-  private static int insertCompanyValues(Company company, Connection conn) throws SQLException {
-    final int companyId;
-    try (PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO company(name) VALUES (?)", Statement.RETURN_GENERATED_KEYS)) {
-      preparedStatement.setString(1, company.name);
-      preparedStatement.executeUpdate();
-
-      // envia un getLong si la generatedKeys existe sino envia una SQLexception
-      companyId = checkIfCompanyIdExist(preparedStatement);
-
-    }
-    return companyId;
-  }
-
-  private static int checkIfCompanyIdExist(PreparedStatement preparedStatement) throws SQLException {
+  static int checkIfCompanyIdExist(PreparedStatement preparedStatement) throws SQLException {
     final int companyId;
     try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
       if (generatedKeys.next()) {
@@ -47,26 +32,6 @@ public class BatchXmlImporterDataBase {
     return companyId;
   }
 
-  private static void insertStaffValues(Connection conn, int companyId, Staff staff) throws SQLException {
-    try (PreparedStatement preparedStatement = conn.prepareStatement(
-      "INSERT INTO staff(id,company_id, first_name, last_name, nick_name) VALUES (?,?,?,?,?)")) {
-      preparedStatement.setInt(1, staff.id);
-      preparedStatement.setInt(2, companyId);
-      preparedStatement.setString(3, staff.firstname);
-      preparedStatement.setString(4, staff.lastname);
-      preparedStatement.setString(5, staff.nickname);
-      preparedStatement.executeUpdate();
-    }
-  }
 
-  private static void insertSalaryValues(Connection conn, Staff staff) throws SQLException {
-    try (PreparedStatement preparedStatement = conn.prepareStatement(
-      "INSERT INTO salary(staff_id, currency, value) VALUES (?,?,?)")) {
-      preparedStatement.setInt(1, staff.id);
-      preparedStatement.setString(2, staff.salary.currency);
-      preparedStatement.setInt(3, staff.salary.value);
-      preparedStatement.executeUpdate();
-    }
-  }
 
 }
---
+++ XMLImporter/src/main/java/BatchXmlImporterInserts.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import xmlmodels.Company;
+import xmlmodels.Staff;
+
+public class BatchXmlImporterInserts {
+  static int insertCompanyValues(Company company, Connection conn) throws SQLException {
+    final int companyId;
+    try (PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO company(name) VALUES (?)", Statement.RETURN_GENERATED_KEYS)) {
+      preparedStatement.setString(1, company.name);
+      preparedStatement.executeUpdate();
+
+      companyId = BatchXmlImporterDataBase.checkIfCompanyIdExist(preparedStatement);
+    }
+    return companyId;
+  }
+
+  static void insertStaffValues(Connection conn, int companyId, Staff staff) throws SQLException {
+    try (PreparedStatement preparedStatement = conn.prepareStatement(
+      "INSERT INTO staff(id,company_id, first_name, last_name, nick_name) VALUES (?,?,?,?,?)")) {
+      preparedStatement.setInt(1, staff.id);
+      preparedStatement.setInt(2, companyId);
+      preparedStatement.setString(3, staff.firstname);
+      preparedStatement.setString(4, staff.lastname);
+      preparedStatement.setString(5, staff.nickname);
+      preparedStatement.executeUpdate();
+    }
+  }
+
+  static void insertSalaryValues(Connection conn, Staff staff) throws SQLException {
+    try (PreparedStatement preparedStatement = conn.prepareStatement(
+      "INSERT INTO salary(staff_id, currency, value) VALUES (?,?,?)")) {
+      preparedStatement.setInt(1, staff.id);
+      preparedStatement.setString(2, staff.salary.currency);
+      preparedStatement.setInt(3, staff.salary.value);
+      preparedStatement.executeUpdate();
+    }
+  }
+}
