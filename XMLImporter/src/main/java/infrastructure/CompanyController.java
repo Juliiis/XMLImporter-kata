49a01acdfed019aae59a17fac65667cebab237d6
julieta <julieta.martin@clarity.ai>
2022-11-08 22:27:18 +0100

    refactor: removed unused imports

--- XMLImporter/src/main/java/infrastructure/CompanyController.java
+++ XMLImporter/src/main/java/infrastructure/CompanyController.java
@@ -1,11 +1,8 @@
 package infrastructure;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import xmlmodels.Company;
 
 public class CompanyController {
   static int findIfTheLastIdExist(PreparedStatement preparedStatement) throws SQLException {
